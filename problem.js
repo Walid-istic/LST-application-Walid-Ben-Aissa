198c8ecdb1fc6a11b74c0d3b4228b57831f41a07
Walid-istic <walid.benaissa@isticbc.org>
2021-04-17 14:36:14 +0100

    v1

---
+++ problem.js
@@ -0,0 +1,50 @@
+var x
+function problem(){
+    t=[]
+    longest=""
+    longesti=0
+    len=0
+    console.log(t);
+    len=Math.floor(Math.random() * 10)+4
+    for(let i=0;i<len;i++)
+        t.push(Math.floor(Math.random() * 2))
+    for(let i=0;i<len;i++){
+        if(t[i]==0){
+            t[i]=1
+            replaceZero(t,i)
+            t[i]=0
+        }
+    }
+    console.log()
+    console.log("the longest :"+longest+" the longest index: "+longesti)
+    return t
+}
+
+function replaceZero(t,i){
+    w=""
+    y=[]
+    for(let j=0;j<=len;j++){
+        if(t[j]==1)
+        w+=t[j]
+        if((t[j]==0)||(j==len-1)){
+            if (w==''){}
+            else{
+            y.push(w)
+            w=""}
+    }
+    }
+    longestOnes(y,i)
+
+    return y
+
+}
+
+function longestOnes(y,i){
+    for(let j=0;j<y.length;j++){
+       if(y[j].length>=longest.length){
+            longest=y[j]
+            longesti=i
+        }
+    }
+}
+console.log(problem())
