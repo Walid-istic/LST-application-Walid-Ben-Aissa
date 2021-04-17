739a48eb3f5be567077dbc50d8d9c7146d1050e1
Walid-istic <walid.benaissa@isticbc.org>
2021-04-17 17:35:57 +0100

    v

--- problem.js
+++ problem.js
@@ -1,47 +1,48 @@
 var x
-function problem(){
-    t=[]
-    longest=""
-    longesti=0
-    len=0
-    len=Math.floor(Math.random() * 10)+4
-    for(let i=0;i<len;i++)
+
+function problem() {
+    t = []
+    longest = ""
+    longesti = 0
+    len = 0
+    len = Math.floor(Math.random() * 10) + 4
+    for (let i = 0; i < len; i++)
         t.push(Math.floor(Math.random() * 2))
-    for(let i=0;i<len;i++){
-        if(t[i]==0){
-            t[i]=1
-            replaceZero(t,i)
-            t[i]=0
+    for (let i = 0; i < len; i++) {
+        if (t[i] == 0) {
+            t[i] = 1
+            replaceZero(t, i)
+            t[i] = 0
         }
     }
-    console.log("the longest :"+longest+" the longest index: "+longesti)
+    console.log("the longest :" + longest + " the longest index: " + longesti)
     return t
 }
 
-function replaceZero(t,i){
-    w=""
-    y=[]
-    for(let j=0;j<=len;j++){
-        if(t[j]==1)
-        w+=t[j]
-        if((t[j]==0)||(j==len-1)){
-            if (w==''){}
-            else{
-            y.push(w)
-            w=""}
+function replaceZero(t, i) {
+    w = ""
+    y = []
+    for (let j = 0; j <= len; j++) {
+        if (t[j] == 1)
+            w += t[j]
+        if ((t[j] == 0) || (j == len - 1)) {
+            if (w == '') {} else {
+                y.push(w)
+                w = ""
+            }
+        }
     }
-    }
-    longestOnes(y,i)
+    longestOnes(y, i)
 
     return y
 
 }
 
-function longestOnes(y,i){
-    for(let j=0;j<y.length;j++){
-       if(y[j].length>=longest.length){
-            longest=y[j]
-            longesti=i
+function longestOnes(y, i) {
+    for (let j = 0; j < y.length; j++) {
+        if (y[j].length >= longest.length) {
+            longest = y[j]
+            longesti = i
         }
     }
 }
